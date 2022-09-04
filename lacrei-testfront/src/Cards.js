a2309353a67282e6ddd418bd1106e82c80e233e3
Nicole Prim <nicolepprim@gmail.com>
2022-09-04 18:21:02 -0300

    desafio concluido

--- lacrei-testfront/src/Cards.js
+++ lacrei-testfront/src/Cards.js
@@ -1,29 +1,38 @@
-import React, { useState } from "react";
-import { Box } from 'wabi-sabi';
+import React, { useState, useEffect } from "react";
+import Box from '@mui/material/Box';
+import BounceLoader from "react-spinners/BounceLoader";
+import { Container } from "@mui/material";
 
-const Cards = ({ color, duration }) => {
+const Cards = (props) => {
   const [background, setBackground] = useState("transparent");
- // const { colors } = getTheme();
+  const [message, setMessage] = useState(false)
 
-  function changeColor() {
-    setBackground(color);
-  }
-
-  setInterval(changeColor, duration);
+  useEffect(() => {
+    setTimeout(() => {
+      setBackground(props.color);
+      setMessage(true)
+    }, props.timer);
+  }, [props.color, props.timer]);
 
   return (
-    <Box
-      alignItems="center"
-      bg={background}
-    /*   border={`5px solid ${colors[color]}`} */
-      margin="5px"
-      borderRadius="10px"
-      display="flex"
-      height={100}
-      justifyContent="center"
-    >
-{/*       <Loader color={colors[color]} size={30} /> */}
-    </Box>
+    <>
+      <Box
+        sx={{
+          alignItems: 'center',
+          background: background,
+          border: `5px solid ${props.color}`,
+          margin: '5px',
+          borderRadius: '10px',
+          width: '50em',
+          height: '5em',
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        {background === 'transparent' ? <BounceLoader color={props.color} size={30} /> : ""}
+        {message && <p><strong>Duração do ciclo</strong> = {props.timer} ms</p>}
+      </Box>
+    </>
   );
 };
 
--- lacrei-testfront/src/Panel.js
+++ lacrei-testfront/src/Panel.js
@@ -1,11 +1,22 @@
 import Cards from "./Cards";
-
+import React from 'react';
+import data from './data.json'
+import { Container } from "@mui/material";
 
 function Panel() {
+
+  const newArray = data.steps.map((box) => {
+    return { ...box, duration: data.durations[`${box.key}`] };
+  });
+
   return (
-    <div>
-      <Cards />
-    </div>
+    <Container sx={{alignContent: 'center', justifyContent: 'center', textAlign: 'center', justifyItems: 'center'}} >
+      <h1>Cards and timer</h1>
+      {newArray.map((card) => {
+        return <Cards color={card.value} timer={card.duration} newArray={newArray}/>
+      })}
+
+    </Container>
   );
 }
 
